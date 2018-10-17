75a7720b0876c5df5ec3a3603e2dadd43f035bab
Daniel Takahashi <takahasd@oregonstate.edu>
2018-10-17 15:24:54 -0700

    Added variance compliance. Still no comments.

--- sobel.py
+++ sobel.py
@@ -8,11 +8,16 @@ import math
 from PIL import Image
 import os
 from os import system
-cwd = os.getcwd()
+cwd = os.getcwd();
+os.system('python3 variance.py');
 print(cwd);#this is the directory you need to put the images in
 path = str(input("File Path?"));
 save = str(input("Save Sobel image as?"));
+rewidth = 640;
+hsize = 480;
 bushes = Image.open(path); #open the image o be sobel-ed
+bushes = bushes.resize((rewidth,hsize),Image.ANTIALIAS);
+bushes.save("COPY.jpg");
 bushes = bushes.convert('RGB');#make sure it's the right type; without this line, some .png files fail
 max_intensity = 0; #keep track of maximum value for scaling purposes
 width, height = bushes.size; #get size of image
@@ -94,8 +99,11 @@ gridx = [];#initializing grid starting point arrays
 gridy = [];
 gridx.append(0);#zero elements
 gridy.append(0);
-threshold = 20;#intensity threshold for recognizing values
-bushid = bushes;
+threshold = 30;#intensity threshold for recognizing values, set to 255 to ignore sobel
+bushid = Image.open("COPY.jpg");
+bushid.save("COPY2.jpg");
+compliance = Image.open("COPY2.jpg");
+bushid = bushid.convert('RGB');
 greenmax = 100;
 print("Sobel imaging complete.");
 f = open("variance.txt","r");
@@ -104,47 +112,62 @@ center_green = f.readline();
 center_blue = f.readline();
 variance_avg = f.readline();
 variance_max = f.readline();
+f.close();
 print("Reading variance data...");
-print("Target center is R" + center_red + " G" + center_green + " B" + center_blue + ".");
-print("Average variance is " + variance_avg + ".");
-print("Maximum variance is " + variance_max + ".");
-print("Establishing position grid...");
+print("Target center is R" + str(int(center_red)) + " G" + str(int(center_green))+ " B"+str(int(center_blue))+".");
+print("Average variance is " + str(int(variance_avg)) + ".");
+print("Maximum variance is " + str(int(variance_max)) + ".");
+print("Establishing grid...");
 g = 0;
 while width > gridx[-1] + 2*incrementx:
     gridx.append(math.floor(gridx[g-1]+incrementx));#For each grid space, initialize x and y values
     g+=1;
 g = 0;
+gridcount = 0;
 while height > gridy[-1] + 2*incrementy:
     gridy.append(math.floor(gridy[g-1]+incrementy));
     g+=1;
 print("Beginning edge and variance comparison...");
 for x in gridx:#iterating through each grid
     for y in gridy:
+        gridcount +=1;
         bush = True;
+        gridComplianceCount = 0;
         for i in range(0, int(incrementx)):#iterating through each pixel in each grid
             for j in range(0,int(incrementy)):
                 p = sobel.getpixel((x+i,y+j));
                 b = bushes.getpixel((x+i,y+j));
                 variance = math.sqrt((int(center_red)-b[0])*(int(center_red)-b[0]) + (int(center_green) - b[1])*(int(center_green)-b[1]) + (int(center_blue)-b[2])*(int(center_blue)-b[2]));
+                if variance < int(variance_avg):
+                    gridComplianceCount += 1;
                 variance_map.putpixel((int(x+i),int(y+j)),(int(variance),int(variance),int(variance)));
                 if(p[1] > threshold or variance > int(variance_max)):#if the pixel is too intense or if theres too much green intensity in the original image
                     bush = False;
+        gridCompliance = int(100*float(gridComplianceCount/24));
+        if(gridCompliance < 2):
+            bush = False;
         if bush == True:
             for i in range(0,int(incrementx)):#iterate through each pixel again to color them
                 for j in range(0,int(incrementy)):
-                    bushid.putpixel((int(x+i),int(y+j)),(int(255),int(0),int(0)));#color grid red
+                    bushid.putpixel((int(x+i),int(y+j)),(255,0,0));#color grid red
+        for i in range(0,int(incrementx)):
+                for j in range(0,int(incrementy)):
+                        compliance.putpixel((int(x+i),int(y+j)),(int(255*float(gridCompliance)/100),int(255*float(gridCompliance)/100),int(255*float(gridCompliance)/100)));
+    print("Completed " + str(gridcount) + " grids out of 50000...");
 print("Finished.");
 sobel.save(save);
-greenscale.save("greenscale.jpg");#save all images
-bluescale.save("bluescale.jpg");
-redscale.save("redscale.jpg");
+#greenscale.save("greenscale.jpg");#save all images
+#bluescale.save("bluescale.jpg");
+#redscale.save("redscale.jpg");
 bushid.save("bushid.jpg");
 print("Maximum intensity in sobel image is " + str(max_intensity) + ".");#some good info to have
 print("Sobel image saved as " + save + " in current directory.");
-bushes.show();
+bushid.show();
 sobel.show();
 variance_map.show();
 variance_map.save("variance.jpg");
+compliance.show();
+compliance.save("compliance.jpg");
 #greenscale.show();#show all images
 #redscale.show();
 #bluescale.show();

9f0c631213af38d17f30383fdc2e2c416cf7043a
Daniel Takahashi <takahasd@oregonstate.edu>
2018-11-04 20:54:41 -0800

    added comments to cpp

--- recog_threaded.cpp
+++ recog_threaded.cpp
@@ -6,12 +6,12 @@
 #include<thread>
 using namespace std;
 using namespace cv;
-class image
+class image		//image object.
 {
 	public:
-		int p[3];
-		Mat matrix;
-		void open(string image)
+		int p[3];//holds the current pixel [r,g,b]
+		Mat matrix;	//actual image matrix
+		void open(string image)	//open an image
 		{
 			matrix = imread(image, CV_LOAD_IMAGE_COLOR);
 			if(!matrix.data)
@@ -19,7 +19,7 @@ class image
 				cout<<"File does not exist."<<endl;
 			}
 		}
-		void get_pixel(int x,int y)
+		void get_pixel(int x,int y)//get a pixel, put it in p, since c sucks at returning arrays. 
 		{
 			cv::Vec3b pixel;
 			pixel = matrix.at<cv::Vec3b>(y,x);
@@ -30,7 +30,7 @@ class image
 			p[1] = int(g);
 			p[2] = int(b);
 		}
-		void set_pixel(int x, int y, int r, int g, int b)
+		void set_pixel(int x, int y, int r, int g, int b)//set a pixel
 		{
 			cv::Vec3b pixel;
 			pixel[2] = uchar(r);
@@ -38,12 +38,12 @@ class image
 			pixel[0] = uchar(b);
 			matrix.at<cv::Vec3b>(y,x) = pixel;
 		}
-		image crop(int x1, int y1, int x2, int y2)
+		image crop(int x1, int y1, int x2, int y2)//crop a certain part of the image. returns a new, cropped image. original image is intact. 
 		{
 			image new_img;
-			Mat blank((y2-y1),(x2-x1),CV_8UC3,Scalar(0,0,0));
+			Mat blank((y2-y1),(x2-x1),CV_8UC3,Scalar(0,0,0));	//new blank
 			new_img.matrix = blank;
-			for(int i = x1;i<x2;i++)
+			for(int i = x1;i<x2;i++)	//fill
 			{
 				for(int j=y1;j<y2;j++)
 				{
@@ -52,27 +52,27 @@ class image
 			}
 			return new_img;
 		}
-		image re_size(int width,int height)
+		image re_size(int width,int height)//resize an image. returns new image. original image is intact.
 		{
 			image resized;
-			cv::resize(matrix,resized.matrix,cv::Size(width,height));
+			cv::resize(matrix,resized.matrix,cv::Size(width,height));//opencv is really good at making simple things kind of ugly.
 			return resized;
 		}	
 	
 };		
-int sample_check(string var_test,int* r, int* g, int* b, int* var_av,int* var_ma)
+int sample_check(string var_test,int* r, int* g, int* b, int* var_av,int* var_ma)//analyzes sample texture. 
 {
 	image img;
-	img.open(var_test);
+	img.open(var_test);//open sample image
 	int avg_b;
 	int avg_r;
 	int avg_g;
 	int var_max=0;
 	int var_avg;
 	int var_sum;
-	int width = img.matrix.size().width;
+	int width = img.matrix.size().width;//keep tabs of size
 	int height = img.matrix.size().height;
-	for(int x=0;x<width;x++)
+	for(int x=0;x<width;x++)//these loops find the center. sum colors of all pixels
 	{
 		for(int y=0;y<height;y++)
 		{
@@ -83,27 +83,27 @@ int sample_check(string var_test,int* r, int* g, int* b, int* var_av,int* var_ma
 		}
 	}
 	avg_r = avg_r/(width*height);
-	avg_g = avg_g/(width*height);
+	avg_g = avg_g/(width*height);//average sums to find center
 	avg_b = avg_b/(width*height);
-	for(int x=0;x<width;x++)
+	for(int x=0;x<width;x++)//these loops handle variance.
 	{
 		for(int y=0;y<height;y++)
 		{
 			img.get_pixel(x,y);
-			int dist_r = avg_r - img.p[0];
+			int dist_r = avg_r - img.p[0];//distance on each axis
 			int dist_g = avg_g - img.p[1];
 			int dist_b = avg_b - img.p[2];
-			int variance = sqrt(dist_r*dist_r + dist_g*dist_g + dist_b*dist_b);
-			if(variance > var_max)
+			int variance = sqrt(dist_r*dist_r + dist_g*dist_g + dist_b*dist_b);//pythagorean distance to center
+			if(variance > var_max)//keep tabs of the maximum variance seen
 			{
 				var_max = variance;
 			}
-			var_sum += variance;
+			var_sum += variance;//sum for an average later
 		}
 	}
-	var_avg = var_sum/(height*width);
+	var_avg = var_sum/(height*width);//heres that average
 	*r = avg_r;
-	*g = avg_g;
+	*g = avg_g;//put it where it goes
 	*b = avg_b;
 	*var_av = var_avg;
 	*var_ma = var_max;
@@ -112,58 +112,57 @@ int sample_check(string var_test,int* r, int* g, int* b, int* var_av,int* var_ma
 image img1,img2,img3,img4,var1,var2,var3,var4;
 image featureRecog(image img, int x1, int y1, int x2, int y2,int avg_r, int avg_g,int avg_b, int var_avg, int var_max,int det)
 {
-	image varmap;
+	image varmap;//this was for debugging. creates a b/w image representing variance.
 	Mat map(120,240,CV_8UC3,Scalar(0,0,0));
 	varmap.matrix = map;
 	image delg;
-	delg = img.crop(x1,y1,x2,y2);
+	delg = img.crop(x1,y1,x2,y2);//crop to specified size. 
 	int gridComplianceCount = 0;
 	int gridCount = 0;
-	bool bush;
+	bool bush;//im aware this has a confusing name.
 	int variance;
 	float gridCompliance;
-	for(int x=0;x<delg.matrix.size().width/4;x++)
+	for(int x=0;x<delg.matrix.size().width/4;x++)//divide into four pixel by two pixel grid. 
 	{	
 		for(int y=0;y<delg.matrix.size().height/2;y++)
 		{
-			gridCount+=1;
-			bush = true;
+			gridCount+=1;//count this grid
+			bush = true;//default to yes, this is a bush
 			gridComplianceCount = 0;
-			for(int i=0;i<4;i++)
+			for(int i=0;i<4;i++)//iterate through each pixel in grid. 
 			{
 				for(int j=0;j<2;j++)
 				{
-					delg.get_pixel(4*x+i,2*y+j);
-					variance = sqrt((avg_r-delg.p[0])*(avg_r-delg.p[0]) + (avg_g - delg.p[1])*(avg_g-delg.p[1]) + (avg_b-delg.p[2])*(avg_b-delg.p[2]));
-					varmap.set_pixel(4*x+i,2*y+j,variance,variance,variance);
-					if(variance<var_avg)
+					delg.get_pixel(4*x+i,2*y+j);//get the pixel
+					variance = sqrt((avg_r-delg.p[0])*(avg_r-delg.p[0]) + (avg_g - delg.p[1])*(avg_g-delg.p[1]) + (avg_b-delg.p[2])*(avg_b-delg.p[2]));//again, pythagorean distance to center.
+					varmap.set_pixel(4*x+i,2*y+j,variance,variance,variance);//put a pixel in the variance map
+					if(variance<var_avg)//if the pixel complies
 					{
-						gridComplianceCount += 1;
+						gridComplianceCount += 1;//count it
 					}
-					if(variance>var_max)
+					if(variance>var_max)//if its waaaay too far off, its not a bush.
 					{
-						//cout<<"Should have found a non-bush"<<endl;
 						bush = false;
 					}
 				}
 			}
-			if(gridComplianceCount<1)
+			if(gridComplianceCount<1)//this used to be more sophisticated. if none fell within the average distance
 			{
-				bush = false;
+				bush = false;//probably not a bush
 			}
-			if(bush==true)
+			if(bush==true)//if it is a bush
 			{
 				for(int i=0;i<4;i++)
 				{
 					for(int j=0;j<2;j++)
 					{
-						delg.set_pixel(4*x+i,2*y+j,255,0,0);
+						delg.set_pixel(4*x+i,2*y+j,255,0,0);//make it a red bush
 					}
 				}
 			}		
 		}
 	}
-	if(det==1)
+	if(det==1)//im aware gloabl variables are the devil but it was so much easier this way. 
 	{
 		img1 = delg;
 		var1 = varmap;
@@ -183,16 +182,16 @@ image featureRecog(image img, int x1, int y1, int x2, int y2,int avg_r, int avg_
 		img4 = delg;
 		var4 = varmap;
 	}
-	return delg;
+	return delg;//return it for fun. I used to use this before it was multithreaded 
 }
-image stitch(image img1, image img2, image img3, image img4)
+image stitch(image img1, image img2, image img3, image img4)//puts images back together after theyre taken apart by the threads
 {
 	Mat blank(240,480,CV_8UC3,Scalar(0,0,0));
 	for(int x=0;x<240;x++)
 	{
 		for(int y=0;y<120;y++)
 		{
-			blank.at<Vec3b>(y,x) = img1.matrix.at<Vec3b>(y,x);
+			blank.at<Vec3b>(y,x) = img1.matrix.at<Vec3b>(y,x);//only works for the right size image. whatever, its a specific application
 			blank.at<Vec3b>(y,x+240) = img2.matrix.at<Vec3b>(y,x);
 			blank.at<Vec3b>(y+120,x) = img3.matrix.at<Vec3b>(y,x);
 			blank.at<Vec3b>(y+120,x+240) = img4.matrix.at<Vec3b>(y,x);
@@ -200,50 +199,39 @@ image stitch(image img1, image img2, image img3, image img4)
 	}
 	image img;
 	img.matrix = blank;
-	return img;
+	return img;//return complete image
 }
 int main()
 {
 	int red,green,blue,v_avg,v_max;
 	int *r = &red;
-	int *g = &green;
+	int *g = &green;//threads really dont like being passed things by reference. Ideally id just hand it &red, etc, but it loves throwing SIGABRT when i do that. 
 	int *b = &blue;
 	int *var_max = &v_max;
 	int *var_avg = &v_avg;
-	string name1,name2;
-	cout<<"Sample?"<<endl;
+	string name1,name2;//filenames
+	cout<<"Sample?"<<endl;//sample file
 	cin>>name1;
-	cout<<"Image?"<<endl;
+	cout<<"Image?"<<endl;//actual image file
 	cin>>name2;
-	sample_check(name1,r,g,b,var_avg,var_max);
+	sample_check(name1,r,g,b,var_avg,var_max);//get sample data
 	image img;
-	img.open(name2);
-	image resized = img.re_size(480,240);
-	thread first (featureRecog,resized,0,0,240,120,*r,*g,*b,*var_avg,*var_max,1);
-	thread second (featureRecog,resized,0,120,240,240,*r,*g,*b,*var_avg,*var_max,2);
-	thread third (featureRecog,resized,240,0,480,120,*r,*g,*b,*var_avg,*var_max,3);
-	thread fourth (featureRecog,resized,240,120,480,240,*r,*g,*b,*var_avg,*var_max,4);
-	first.join();
+	img.open(name2);//open main image
+	image resized = img.re_size(480,240);//resize main image
+	thread first (featureRecog,resized,0,0,240,120,*r,*g,*b,*var_avg,*var_max,1);//start first thread. upper left quarter of image. 
+	thread second (featureRecog,resized,0,120,240,240,*r,*g,*b,*var_avg,*var_max,2);//start second thread. lower left quarter of image.
+	thread third (featureRecog,resized,240,0,480,120,*r,*g,*b,*var_avg,*var_max,3);//start third thread. upper right quarter of image.
+	thread fourth (featureRecog,resized,240,120,480,240,*r,*g,*b,*var_avg,*var_max,4);//start fourth thread. lower left quarter of image. 
+	first.join();//join all the threads. 
 	second.join();
 	third.join();
 	fourth.join();
-	image done = stitch(img1,img3,img2,img4);
+	image done = stitch(img1,img3,img2,img4);//make it whole again. 
 	image varmap = stitch(var1,var3,var2,var4);
-	namedWindow("Done",WINDOW_AUTOSIZE);
-	imshow("Done",done.matrix);
+	namedWindow("Done",WINDOW_AUTOSIZE);//make windows. opencv sucks. 
+	imshow("Done",done.matrix);//show it
 	namedWindow("varmap",WINDOW_AUTOSIZE);
 	imshow("varmap",varmap.matrix);
-	waitKey(0);
-	VideoCapture cap(0);
-	Mat frame;
-	while(true)
-	{
-		cap.read(frame);
-		imshow("camera",frame);
-		if(waitKey(30)==27)
-		{
-			return 0;
-		}
-	}
+	waitKey(0);//wait until a key is pressed. 
 	return 0;
 }

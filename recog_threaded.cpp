c61e4e6f7535d542966cf98e56b116e80eb71dd8
Daniel Takahashi <takahasd@oregonstate.edu>
2018-11-04 18:24:17 -0800

    now the c++ works.

--- recog_threaded.cpp
+++ recog_threaded.cpp
@@ -3,6 +3,7 @@
 #include<string>
 #include<iostream>
 #include<unistd.h>
+#include<thread>
 using namespace std;
 using namespace cv;
 class image
@@ -40,13 +41,13 @@ class image
 		image crop(int x1, int y1, int x2, int y2)
 		{
 			image new_img;
-			Mat blank(120,240,CV_8UC3,Scalar(0,0,0));
+			Mat blank((y2-y1),(x2-x1),CV_8UC3,Scalar(0,0,0));
 			new_img.matrix = blank;
 			for(int i = x1;i<x2;i++)
 			{
 				for(int j=y1;j<y2;j++)
 				{
-					new_img.matrix.at<cv::Vec3b>(j,i) = matrix.at<Vec3b>(j,i);
+					new_img.matrix.at<cv::Vec3b>(j-y1,i-x1) = matrix.at<Vec3b>(j,i);
 				}
 			}
 			return new_img;
@@ -59,7 +60,7 @@ class image
 		}	
 	
 };		
-int sample_check(string var_test)
+int sample_check(string var_test,int* r, int* g, int* b, int* var_av,int* var_ma)
 {
 	image img;
 	img.open(var_test);
@@ -101,20 +102,19 @@ int sample_check(string var_test)
 		}
 	}
 	var_avg = var_sum/(height*width);
-	cout<<"Image is centered at R";
-	cout<<avg_r;
-	cout<<" G";
-	cout<<avg_g;
-	cout<<" B";
-	cout<<avg_b<<endl;
-	cout<<"Average variance is ";
-	cout<<var_avg<<endl;
-	cout<<"Max variance is ";
-	cout<<var_max<<endl;
+	*r = avg_r;
+	*g = avg_g;
+	*b = avg_b;
+	*var_av = var_avg;
+	*var_ma = var_max;
 
 }
-void featureRecog(image img, int x1, int y1, int x2, int y2,int avg_r, int avg_g,int avg_b, int var_avg, int var_max)
+image img1,img2,img3,img4,var1,var2,var3,var4;
+image featureRecog(image img, int x1, int y1, int x2, int y2,int avg_r, int avg_g,int avg_b, int var_avg, int var_max,int det)
 {
+	image varmap;
+	Mat map(120,240,CV_8UC3,Scalar(0,0,0));
+	varmap.matrix = map;
 	image delg;
 	delg = img.crop(x1,y1,x2,y2);
 	int gridComplianceCount = 0;
@@ -122,9 +122,9 @@ void featureRecog(image img, int x1, int y1, int x2, int y2,int avg_r, int avg_g
 	bool bush;
 	int variance;
 	float gridCompliance;
-	for(int x=0;x<((x2-x1)-4)/4;x++)
+	for(int x=0;x<delg.matrix.size().width/4;x++)
 	{	
-		for(int y=0;y<((y2-y1)-2)/2;y++)
+		for(int y=0;y<delg.matrix.size().height/2;y++)
 		{
 			gridCount+=1;
 			bush = true;
@@ -135,7 +135,7 @@ void featureRecog(image img, int x1, int y1, int x2, int y2,int avg_r, int avg_g
 				{
 					delg.get_pixel(4*x+i,2*y+j);
 					variance = sqrt((avg_r-delg.p[0])*(avg_r-delg.p[0]) + (avg_g - delg.p[1])*(avg_g-delg.p[1]) + (avg_b-delg.p[2])*(avg_b-delg.p[2]));
-					cout<<variance<<endl;
+					varmap.set_pixel(4*x+i,2*y+j,variance,variance,variance);
 					if(variance<var_avg)
 					{
 						gridComplianceCount += 1;
@@ -147,12 +147,6 @@ void featureRecog(image img, int x1, int y1, int x2, int y2,int avg_r, int avg_g
 					}
 				}
 			}
-			cout<<gridComplianceCount<<endl;
-			gridCompliance = int(100*float(gridComplianceCount/8));
-			cout<<"Grid compliance of grid ";
-			cout<<gridCount;
-			cout<<" is";
-			cout<<gridCompliance<<endl;
 			if(gridComplianceCount<1)
 			{
 				bush = false;
@@ -169,18 +163,87 @@ void featureRecog(image img, int x1, int y1, int x2, int y2,int avg_r, int avg_g
 			}		
 		}
 	}
-	namedWindow("bushid",WINDOW_AUTOSIZE);
-	imshow("bushid",delg.matrix);
-	waitKey(0);
+	if(det==1)
+	{
+		img1 = delg;
+		var1 = varmap;
+	}
+	if(det==2)
+	{
+		img2 = delg;
+		var2 = varmap;
+	}
+	if(det==3)
+	{
+		img3 = delg;
+		var3 = varmap;
+	}
+	if(det==4)
+	{
+		img4 = delg;
+		var4 = varmap;
+	}
+	return delg;
 }
-
+image stitch(image img1, image img2, image img3, image img4)
+{
+	Mat blank(240,480,CV_8UC3,Scalar(0,0,0));
+	for(int x=0;x<240;x++)
+	{
+		for(int y=0;y<120;y++)
+		{
+			blank.at<Vec3b>(y,x) = img1.matrix.at<Vec3b>(y,x);
+			blank.at<Vec3b>(y,x+240) = img2.matrix.at<Vec3b>(y,x);
+			blank.at<Vec3b>(y+120,x) = img3.matrix.at<Vec3b>(y,x);
+			blank.at<Vec3b>(y+120,x+240) = img4.matrix.at<Vec3b>(y,x);
+		}
+	}
+	image img;
+	img.matrix = blank;
+	return img;
+}
 int main()
 {
-	sample_check("variance_test.jpg");
+	int red,green,blue,v_avg,v_max;
+	int *r = &red;
+	int *g = &green;
+	int *b = &blue;
+	int *var_max = &v_max;
+	int *var_avg = &v_avg;
+	string name1,name2;
+	cout<<"Sample?"<<endl;
+	cin>>name1;
+	cout<<"Image?"<<endl;
+	cin>>name2;
+	sample_check(name1,r,g,b,var_avg,var_max);
 	image img;
-	img.open("bushes3.jpg");
+	img.open(name2);
 	image resized = img.re_size(480,240);
-	featureRecog(img,0,0,240,120,70,72,48,8,77);
+	thread first (featureRecog,resized,0,0,240,120,*r,*g,*b,*var_avg,*var_max,1);
+	thread second (featureRecog,resized,0,120,240,240,*r,*g,*b,*var_avg,*var_max,2);
+	thread third (featureRecog,resized,240,0,480,120,*r,*g,*b,*var_avg,*var_max,3);
+	thread fourth (featureRecog,resized,240,120,480,240,*r,*g,*b,*var_avg,*var_max,4);
+	first.join();
+	second.join();
+	third.join();
+	fourth.join();
+	image done = stitch(img1,img3,img2,img4);
+	image varmap = stitch(var1,var3,var2,var4);
+	namedWindow("Done",WINDOW_AUTOSIZE);
+	imshow("Done",done.matrix);
+	namedWindow("varmap",WINDOW_AUTOSIZE);
+	imshow("varmap",varmap.matrix);
 	waitKey(0);
+	VideoCapture cap(0);
+	Mat frame;
+	while(true)
+	{
+		cap.read(frame);
+		imshow("camera",frame);
+		if(waitKey(30)==27)
+		{
+			return 0;
+		}
+	}
 	return 0;
 }
